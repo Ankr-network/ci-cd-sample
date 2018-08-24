79bb34812e7820cfa4bda742d29f352c0fb62ebe
benforest <benforest1980@gmail.com>
2018-08-24 14:22:48 -0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express')
 var app = express()
 app.get('/', function (req, res) {
-  res.send('Next World!!')
-  res.send('Again!!')
+  res.send('Next World!! \n again!')
 })
 app.listen(8080, function () {
   console.log('app listening on port 8080!')
