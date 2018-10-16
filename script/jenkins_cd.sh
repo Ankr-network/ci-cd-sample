d73e539644898a699939ac28e28daffeaee9c3c2
benforest <hanping@ankr.network>
2018-10-16 11:33:11 -0700

    jenkins build

--- script/jenkins_cd.sh
+++ script/jenkins_cd.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-ssh ubuntu@13.57.223.53 <<EOF
- cd ~/ci-cd-sample
- git pull
- npm install
- pm2 restart all
- exit
-EOF
+ssh ubuntu@13.57.223.53 /bin/bash <<'EOT'
+cd ~/ci-cd-sample
+git pull
+npm install
+pm2 restart all
+EOT
 
